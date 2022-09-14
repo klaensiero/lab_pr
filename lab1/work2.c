99b7625bb9004e377fda16b2e3c77c2f651320cc
sneakypanther <66124861+klaensiero@users.noreply.github.com>
2022-09-14 17:20:09 +0300

    Demo change

--- lab1/work2.c
+++ lab1/work2.c
@@ -3,11 +3,11 @@
 
 int main() {
     double m;
-    float z1,z2;
+    float a1,a2;
     printf("Введите число: ");
     scanf("%lf", &m);
-    z1=(sqrt(pow(3*m+2,2)-24*m))/(3*sqrt(m)-(2/sqrt(m)));
-    z2=sqrt(m);
-    printf("%.2f %.2f\n", z1, z2);
+    a1=(sqrt(pow(3*m+2,2)-24*m))/(3*sqrt(m)-(2/sqrt(m)));
+    a2=sqrt(m);
+    printf("%.2f %.2f\n", a1, a2);
     return 0;
 }
