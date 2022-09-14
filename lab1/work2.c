49ed9391a73fa2fc0ba8708a144a166e643c0e58
sneakypanther <66124861+klaensiero@users.noreply.github.com>
2022-09-14 18:27:13 +0300

    More minor fixes

--- lab1/work2.c
+++ lab1/work2.c
@@ -7,7 +7,7 @@ int main() {
     printf("Введите число: ");
     scanf("%lf", &m);
     a1=(sqrt(pow(3*m+2,2)-24*m))/(3*sqrt(m)-(2/sqrt(m)));
-    a2=sqrt(m);
-    printf("%.2f %.2f\n", a1, a2);
+    a2= -sqrt(m);
+    printf("a1 = %f; a2 = %f\n", a1, a2);
     return 0;
 }
