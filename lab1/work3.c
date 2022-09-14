f40a518f3ced9d2b847308afc5bcba2e9a78ea54
sneakypanther <66124861+klaensiero@users.noreply.github.com>
2022-09-14 18:05:42 +0300

    Changed the whole algorithm

--- lab1/work3.c
+++ lab1/work3.c
@@ -3,11 +3,18 @@
 int main() {
     float t1;
     char t2;
-    printf("Введите вещественное число: ");
-    scanf("%f", &t1);
-    printf("Вывод: %f\n", t1);
-    printf("Введите символ: ");
-    scanf("%s", &t2);
-    printf("Вывод: %c\n", t2);
+    int check;
+    printf("Выберите операцию - вывести вещественное число (1) или символ (2): ");
+    scanf("%d", &check);
+    if (check==1) {
+        printf("Введите вещественное число: ");
+        scanf("%f", &t1);
+        printf("Вывод: %f\n", t1);
+    }
+    if (check==2) {
+        printf("Введите символ: ");
+        scanf("%s", &t2);
+        printf("Вывод: %c\n", t2);
+    }
     return 0;
 }
