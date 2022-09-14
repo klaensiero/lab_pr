0ffe85bd364557d8095c3f0ebcb113134311d07f
sneakypanther <66124861+klaensiero@users.noreply.github.com>
2022-09-14 17:46:19 +0300

    Several fixes

--- lab1/work3.c
+++ lab1/work3.c
@@ -2,12 +2,12 @@
 
 int main() {
     float t1;
-    char t2 [100];
+    char t2;
     printf("Введите вещественное число: ");
     scanf("%f", &t1);
-    printf("Вывод: %.1f\n", t1);
+    printf("Вывод: %f\n", t1);
     printf("Введите символ: ");
     scanf("%s", &t2);
-    printf("Вывод: %s\n", t2);
+    printf("Вывод: %c\n", t2);
     return 0;
 }
