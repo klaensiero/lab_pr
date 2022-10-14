7d62f654d71516796243577274d902d56952f5ba
sneakypanther <66124861+klaensiero@users.noreply.github.com>
2022-10-14 16:53:29 +0300

    New additions

--- lab1/work4_1.c
+++ lab1/work4_1.c
@@ -4,8 +4,7 @@ int main() {
     int number;
     printf("Введите число: ");
     scanf("%d", &number);
-    while ((number >= 20) && (number <= 30))
-    {
+    while ((number >= 20) && (number <= 30)) {
         printf("Попал в диапазон\n");
         return 0;
     }
--- lab1/work4_2.c
+++ lab1/work4_2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-
+    int num, num1;
+    scanf("%d",&num1);
+    printf("%d",(num1>>17)%2);
     return 0;
-} 
+}
